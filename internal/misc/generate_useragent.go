65e94b8f7fa79e52f8c5a4c910c73726376458a9
TheDevisi <devisinov@gmail.com>
2026-05-21 20:12:21 +0300

    fix: add credits to user agents & linter fix

--- internal/misc/generate_useragent.go
+++ internal/misc/generate_useragent.go
@@ -1,5 +1,7 @@
 package misc
 
+// user agents are provided by https://github.com/MichaelJorky/top-user-agents-latest/blob/main/combined/random-generator-list.txt
+
 import (
 	_ "embed"
 	"math/rand/v2"
@@ -9,9 +11,10 @@ import (
 //go:embed useragents.txt
 var useragents string
 
-// Provides a random usre agent from the useragents.txt
-func GenerateUserAgent() (user_agent string) {
+// GenerateUserAgen provides a random usre agent from the useragents.txt
+func GenerateUserAgent() (userAgent string) {
 	useragentsArr := strings.Split(useragents, "\n")
 	// i dont think we should return an err because there's no chance it can occur
+	//#nosec G404
 	return useragentsArr[rand.IntN(len(useragentsArr))]
 }
